addf6db2fb459dc7913692be9fbf7e6855a12d57
JaredChinchilla06 <00184924@uca.edu.sv>
2024-09-21 12:58:41 -0600

    :tada: Ejercicios del repositorio de la clase

---
+++ Ejercicios del repositorio/DeclaraciónDeVariables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int entero = 15;
+    char letra = 65;
+    float decimal = 15.5;
+
+    cout << "la variable tipo entera es: " << entero << endl;
+    cout << "La variable tipo caracter es: " << letra << endl;
+    cout << "La variable tipo decimal es: " << decimal << endl;
+
+
+    return 0;
+}
---
+++ Ejercicios del repositorio/OperacionesAritmeticas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+
+    cout << "Ingrese un numero entero: " << endl;
+    cin >> num1;
+
+    cout << "Ingrese un segundo numero entero: " << endl;
+    cin >> num2;
+
+    cout << "La suma de sus numeros enteros es: " << num1 + num2 << endl;
+
+    cout << "La multiplicacion de sus numeros enteros es: " << num1 * num2 << endl;
+
+    cout << "La division de sus numeros enteros es: " << num1 / num2 << endl;
+
+    cout << "La resta de sus numeros enteros es: " << num1 - num2 << endl;
+
+
+    return 0;
+}
